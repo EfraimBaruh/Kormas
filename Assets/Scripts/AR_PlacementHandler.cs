41337199f2bfdcdd6c18c29f54524f6bdca6ec4d
EminAdastec <emin@adastec.com>
2021-12-26 01:28:40 +0300

    ui flow

---
+++ Assets/Scripts/AR_PlacementHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AR_PlacementHandler : MonoBehaviour
+{
+    private GameObject currentModel;
+
+    public PlaceOnPlane placer;
+
+    public void SetCurrentModel()
+    {
+        ProductInfo productInfo = AR_Object_Info.instance.SelectedProduct.productInfo;
+
+        currentModel = productInfo.GetModel();
+        try
+        {
+            placer.placedPrefab = currentModel;
+        }
+        catch (NullReferenceException e)
+        {
+            Debug.LogError("missing model for product");
+        }
+        placer.placedPrefab.GetComponent<ProductManager>().productInfo = productInfo;
+    }
+}
